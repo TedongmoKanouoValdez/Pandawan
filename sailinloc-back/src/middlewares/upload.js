c1ccd245703a2a941f4924e88fa5e86b63a4a37d
valdez Tedongmo <114952948+TedongmoKanouoValdez@users.noreply.github.com>
2025-07-09 14:07:56 +0200

    handle boat creation with uploaded images stored in media table and added availability field

---
+++ sailinloc-back/src/middlewares/upload.js
@@ -0,0 +1,16 @@
+// middlewares/upload.js
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // dossier temporaire
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
+
+module.exports = upload;
