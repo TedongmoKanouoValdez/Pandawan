d42a35e978597b655556aee9c9128677349efc3e
valdez Tedongmo <114952948+TedongmoKanouoValdez@users.noreply.github.com>
2025-07-17 15:32:11 +0200

    feat: integrate user registration and login via backend API

--- sailinloc-back/index.js
+++ sailinloc-back/index.js
@@ -1,16 +1,23 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
-// Middleware pour parser le JSON dans les requêtes
+// Middleware CORS AVANT les routes
+app.use(cors({
+  origin: 'http://localhost:3000', //
+  credentials: true,
+}));
+
+// Middleware pour parser le JSON
 app.use(express.json());
 
 // Importer les routes auth
 const authRoutes = require('./src/routes/auth');
 app.use('/api/auth', authRoutes);
-const bateauRoutes = require('./src/routes/bateauRoute');
-app.use("/api", bateauRoutes); // accès via /api/bateaux
 
+const bateauRoutes = require('./src/routes/bateauRoute');
+app.use('/api', bateauRoutes);
 
 // Démarrer le serveur
 const PORT = process.env.PORT || 3001;
